78ec4068fc94c8d44d44298f954cc8b1cd8f1131
JakobGospoaric1 <jg9837@student.uni-lj.si>
2021-03-21 14:18:02 +0100

    Pogoji hotelov

--- skripte/skripta.js
+++ skripte/skripta.js
@@ -165,7 +165,7 @@ function prikaziHoteleZnotrajRadija() {
 
     // Preveri pogoje: izbrana 2 parka in vsota v osnovnih podatkih večjo od 0
     preveriPogojeSklopaHoteli(function () {
-        var srednjaTocka = -1;
+        var srednjaTocka = middlePoint(parki[kandidatiParkov[0]].koordinate.lat, parki[kandidatiParkov[0]].koordinate.lng, parki[kandidatiParkov[1]].koordinate.lat, parki[kandidatiParkov[1]].koordinate.lng);
 
         // Prikaži hotele glede na radij
         pridobiPodatke("hoteli", function (jsonRezultat, vrstaInteresneTocke) {
@@ -177,7 +177,16 @@ function prikaziHoteleZnotrajRadija() {
             odstraniVseTockeNaZemljevidu();
 
             // Nariši radij (rdeč krog v središčni točki med izbranima parkoma)
-            radijHotelov = undefined;
+            radijHotelov = new L.circle([srednjaTocka[1], srednjaTocka[0]], {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.5,
+                radius: document.getElementById("radij").value*1000,
+                stroke: false
+            });
+            radijHotelov.addTo(mapa);
+
+            izracunajVseHoteleVRadiju(srednjaTocka[1], srednjaTocka[0], document.getElementById("radij").value);
 
             // Poišči vse hotele znotraj radija
 
@@ -240,6 +249,17 @@ function izracunajVseHoteleVRadiju(lat, lng, radij) {
  */
 function preveriPogojeSklopaHoteli(callback) {
     var sporocilo = "";
+
+    if (vsota==0) {
+        sporocilo = "Skupni znesek v osnovnih podatkih mora biti več kot 0 €!";
+        alert(sporocilo);
+    }
+
+    if (kandidatiParkov.length!=2) {
+        sporocilo = "Izbrati je potrebno 2 parka!";
+        alert(sporocilo);
+    }
+    
 
     if (!sporocilo) {
         callback();
